1ce072e5281978249af724cb99cf8c2f9d70ae06
KellyVasconcellos <kelly.h.vasconcellos@gmail.com>
2023-06-24 17:32:37 -0300

    ajustando component brackground-black

--- src/app/acesso-aberto/components/background-preto/background-preto.component.ts
+++ src/app/acesso-aberto/components/background-preto/background-preto.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-background-preto',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./background-preto.component.scss']
 })
 export class BackgroundPretoComponent {
+
+  @Input() primeiraImagem: string = ""
+  @Input() segundaImagem: string = ""
+  @Input() terceiraImagem: string = ""
+  @Input() textoBotao: string =""
 
 }
