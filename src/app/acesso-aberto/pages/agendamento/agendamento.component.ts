9ea17a6a0760efb476e1177f602e1db152a6f490
KellyVasconcellos <kelly.h.vasconcellos@gmail.com>
2023-06-24 21:21:31 -0300

    deixando a data de funcionamento dinamica

--- src/app/acesso-aberto/pages/agendamento/agendamento.component.ts
+++ src/app/acesso-aberto/pages/agendamento/agendamento.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { IHorario } from '../../interface/horario';
+import { DiaHorarioEnum } from '../../enum/diaHorario';
 
 @Component({
   selector: 'app-agendamento',
   templateUrl: './agendamento.component.html',
   styleUrls: ['./agendamento.component.scss']
 })
+
+
 export class AgendamentoComponent {
 
+  horarios: Array<IHorario> = [
+    {
+      dia: "Segunda-Feira",
+      horario: "09:00 - 21:00"
+    },
+    {
+      dia: "Terça-Feira",
+      horario: "09:00 - 21:00"
+    },
+    {
+      dia: "Quarta-Feira",
+      horario: "09:00 - 21:00"
+    },
+    {
+      dia: "Quinta-Feira",
+      horario: "09:00 - 21:00"
+    },
+    {
+      dia: "Sexta-Feira",
+      horario: "09:00 - 21:00"
+    },
+    {
+      dia: "Sábado",
+      horario: "08:00 - 19:00"
+    },
+    {
+      dia: "Domingo",
+      horario: "Fechado"
+    },
+  ];
+
+
+  diaAtual(dia:string): boolean {
+    const data = new Date()
+    const pegaDiaEnum = DiaHorarioEnum[data.getDay()]
+    if (pegaDiaEnum == dia){
+      return true
+    } else {
+      return false
+    }
+  }
 }
