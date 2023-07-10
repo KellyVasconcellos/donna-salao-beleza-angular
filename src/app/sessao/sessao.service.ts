98bac3f1bc9fff9efc587e02a59e7c1c0786cb96
KellyVasconcellos <kelly.h.vasconcellos@gmail.com>
2023-07-10 15:27:28 -0300

    commit finalizando pagina de servico

--- src/app/sessao/sessao.service.ts
+++ src/app/sessao/sessao.service.ts
@@ -8,6 +8,13 @@ export class SessaoService {
 
   CHAVE_SESSAO = "usuario_sessao";
 
+  /**
+   * Ao fazer login devemos guardar o id do usuario na sessao do navegador.
+   * criar um formulario reativo (ver no git da bruxa) para pegar os valores: id imagem e id serviço.
+   * Enviar id imagem e id serviço via serviço subject (criar dentro do seviço de sessao o subject para enviar ids e capturar ids).
+   * ver referencia para subject: https://consolelog.com.br/como-proteger-rotas-angular-com-guard/
+   */
+
   constructor() { }
 
   salvarSessao(sessao: ISessao){
